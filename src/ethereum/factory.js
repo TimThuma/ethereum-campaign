5757158e14e8ee5df58b05ff3ac4a3114e9e4a87
Kai Kang <cctv2206@outlook.com>
2018-04-14 14:47:25 -0700

    deploy upgraded new campaign contract

--- src/ethereum/factory.js
+++ src/ethereum/factory.js
@@ -4,7 +4,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0xCDfAa4e17Ff2b4cFDE0d48869B066d1a65A21B69'
+  '0x89562BE979804d2947D83d4643568ec4b6f2F6B5'
 );
 
 export default instance;
